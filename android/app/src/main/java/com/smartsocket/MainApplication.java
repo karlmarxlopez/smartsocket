84cea23f57b8bc73dff36840dc7deb64b4a380a9
karlmarxlopez <officialkarl.kml@gmail.com>
2019-07-15 13:18:48 +0800

    chore: installed appcenter and friends

--- android/app/src/main/java/com/smartsocket/MainApplication.java
+++ android/app/src/main/java/com/smartsocket/MainApplication.java
@@ -3,6 +3,9 @@ package com.smartsocket;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.database.RNFirebaseDatabasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +29,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new RNFirebasePackage(),
             new RNFirebaseDatabasePackage(),
             new VectorIconsPackage()
